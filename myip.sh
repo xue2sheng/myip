63fbad77ac5d8a104ac7d519c9bbdab77b8bedef
xue2sheng <andres.sevillano.vallejo@gmail.com>
2019-05-19 01:01:50 +0100

    More HTTPS friendly

--- myip.sh
+++ myip.sh
@@ -2,5 +2,8 @@
 /usr/bin/sleep 10s
 /usr/bin/dig TXT +short o-o.myaddr.google.com @ns1.google.com > /home/user/archlinux.txt
 /usr/bin/sed -i -e 's/"//g' /home/user/archlinux.txt
+/usr/bin/cp /home/user/archlinux.txt /home/user/index.html
+/usr/bin/sed -i -e 's/\(.*\)/<!DOCTYPE html><html><head><title>Archlinux<\/title><\/head><body><a href="https:\/\/[\1]">\1<\/a><\/body><\/html>/g' /home/user/index.html
 /usr/bin/scp /home/user/archlinux.txt user@xue2sheng.com:/home/user/archlinux.txt
-# expected softlink at the server archlinux.txt --> /var/www/desktop/index.html
+/usr/bin/scp /home/user/index.html user@xue2sheng.com:/home/user/index.html
+# expected softlink at the server index.html --> /var/www/desktop/index.html
